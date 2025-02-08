8da3ea18337a4cf111f6450a1e5754c73ee90b65
Benjamin Lee <benjaminlee755@gmail.com>
2025-02-08 15:29:36 -0500

    .

---
+++ hacknyu/src/ui/UserInterface.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './UserInterface.module.css';
+
+function UserInterface() {
+  return (
+    <>
+      hi
+    </>
+  )
+}
+
+export default UserInterface;
