6d4d23cfc414e3f3881872538321d8cef9310dc4
WangWNico <nicowang0404@gmail.com>
2025-02-08 16:11:23 -0500

    Idk it broke

--- hacknyu/src/main.jsx
+++ hacknyu/src/main.jsx
@@ -2,11 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import DungeonMasterTest from './DungeonMasterTest.jsx'
+import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <>
+      <div style={{ color: 'white' }}>Debug: App Component</div>
+      <App />
+      <div style={{ color: 'white' }}>Debug: DungeonMasterTest Component</div>
       <DungeonMasterTest />
     </>
-  </StrictMode>,
+  </StrictMode>
 )
