38c51eead94993b2886dac818eca0e2243021b55
Benjamin Lee <benjaminlee755@gmail.com>
2025-02-08 17:30:03 -0500

    Update Storybox.jsx

--- hacknyu/src/ui/Storybox.jsx
+++ hacknyu/src/ui/Storybox.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import styles from './Storybox.module.css';
 
 function StoryBox() {
-    const generatedText = "This is the computer-generated text. It can be long and will wrap automatically.  It can contain multiple lines and will expand the box vertically as needed."
+    const generatedText = "Welcome Adventurers..."
     return (
         <>
         <div className={styles.container}>
