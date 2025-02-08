788fd4334af5f908c52827a0965470909f60ab13
Benjamin Lee <benjaminlee755@gmail.com>
2025-02-08 17:15:08 -0500

    added custom response component

--- hacknyu/src/ui/UserInterface.jsx
+++ hacknyu/src/ui/UserInterface.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import styles from './UserInterface.module.css';
 import Optionsbox from './Optionsbox.jsx'
 import Storybox from './Storybox.jsx'
+import CustomResponse from './CustomResponse.jsx'
 
 function UserInterface() {
   return (
     <>
       <Storybox />
       <Optionsbox />
+      <CustomResponse />
     </>
   )
 }
