15cc78fba9247f00795fd4ff6fb8da3473ec4298
Benjamin Lee <benjaminlee755@gmail.com>
2025-02-08 17:13:07 -0500

    created and styled the option boxes

--- hacknyu/src/ui/Optionsbox.jsx
+++ hacknyu/src/ui/Optionsbox.jsx
@@ -2,11 +2,18 @@ import React from 'react';
 import styles from './Optionsbox.module.css';
 
 function OptionsBox() {
-  return (
-    <>
-      hi
-    </>
-  )
+    return (
+        <>
+        <div className={styles.optionsBox}>
+            <div className={styles.options}>
+                <button className={styles.optionButton}>Option 1</button>
+                <button className={styles.optionButton}>Option 2</button>
+                <button className={styles.optionButton}>Option 3</button>
+                <button className={styles.optionButton}>Option 4</button>
+            </div>
+        </div>
+        </>
+    )
 }
 
 export default OptionsBox;
