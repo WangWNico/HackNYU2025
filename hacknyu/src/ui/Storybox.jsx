57d9aa49094834719ee40f56985223f682872884
Benjamin Lee <benjaminlee755@gmail.com>
2025-02-08 17:12:53 -0500

    Created and styled the storybox

--- hacknyu/src/ui/Storybox.jsx
+++ hacknyu/src/ui/Storybox.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
-import styles from './UserInterface.module.css';
+import styles from './Storybox.module.css';
 
 function StoryBox() {
-  return (
-    <>
-      hi
-    </>
-  )
+    const generatedText = "This is the computer-generated text. It can be long and will wrap automatically.  It can contain multiple lines and will expand the box vertically as needed."
+    return (
+        <>
+        <div className={styles.container}>
+            <div className={styles.textBox}>
+                <p className={styles.textDisplay}>{generatedText}</p>
+            </div>
+        </div>
+        </>
+    )
 }
 
 export default StoryBox;
