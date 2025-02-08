2600aa4884df4b0c1129db3bc87c5f272fca8615
NaffodA <edmondhl004@gmail.com>
2025-02-08 14:04:58 -0500

    ui start

---
+++ hacknyu/src/ui/UserInterface.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './UserInterface.module.css';
+
+function UserInterface() {
+  return (
+    <div>UserInterface</div>
+  )
+}
+
+export default UserInterface
